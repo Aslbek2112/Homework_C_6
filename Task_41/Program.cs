2a36e57d2a4486e9c49ef4dc6e8d55ae6a284ee2
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-07-04 16:58:08 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите количество чисел, которое вы хотите ввести: ");
+        int count = int.Parse(Console.ReadLine());
+
+        int positiveCount = 0;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Введите число №{i + 1}: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел, больше 0: {positiveCount}");
+
+        Console.ReadLine();
+    }
+}
+
